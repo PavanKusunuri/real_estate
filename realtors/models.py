32b63a56d1d9d6361aa36580c6a2c0a3f26e63a2
Pavan <pavantejakusunuri@gmail.com>
2020-04-02 19:15:22 +0530

    Done Migrations for listings and relators models

--- realtors/models.py
+++ realtors/models.py
@@ -9,5 +9,5 @@ class Realtor(models.Model):
     email = models.CharField(max_length=50)
     is_mvp = models.BooleanField(default=False)
     hire_date = models.DateTimeField(default=datetime.now, blank=True)
-    def__str__(self):
+    def __str__(self):
       return self.name 
